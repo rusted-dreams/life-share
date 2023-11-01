44fe33ffa0304ace07381407dccaac4b9147024b
gyash1512 <gyash.1512@gmail.com>
2023-11-01 19:31:55 +0530

    Updated addDonor

--- addDonor.cpp
+++ addDonor.cpp
@@ -2,180 +2,167 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <ctime>   // For time functions
+#include <iomanip> // For date/time formatting
+#include <vector>
 
-struct Donor {
-    std::string name;
-    std::string organType;
-    std::string contact;
-    int age;
-    std::string bloodType;
-    std::string regDate;
-    std::string city;
-    std::string state;
-    std::string country;
-    std::string address;
-    std::string zipCode;
-    std::string medicalHistory;
-    std::string insurance;
-    std::string consentForm;
+using namespace std;
+
+const string CSV_FILE_PATH = "receiver.csv";
+
+struct ReceiverDetails {
+    string ReceiverID;
+    string ReceiverName;
+    string ReceiverContact;
+    string OrganType;
+    string ReceiverAge;
+    string ReceiverBloodType;
+    string ReceiverAddress;
+    string ReceiverCity;
+    string ReceiverState;
+    string ReceiverCountry;
+    string ReceiverZipCode;
+    string ReceiverMedicalHistory;
+    string ReceiverInsurance;
+    string ReceiverConsentForm;
+    string ReceiverRegistrationDate;
 };
 
-void registerDonor(std::ofstream& file, Donor& newDonor);
-void printDonorDetails(const Donor& donor);
-bool confirmRegistration();
-void runDonorRegistrationSystem();
+vector<ReceiverDetails> receiverDatabase;  // Vector to store receiver details
 
-int main() {
-    std::cout << "Do you want to register as a donor? (Y/N): ";
-    char registrationChoice;
-    std::cin >> registrationChoice;
+string generate_receiver_id(const ReceiverDetails& receiver) {
+    // Extract relevant components for ReceiverID
+    string first_letter = receiver.ReceiverName.substr(0, 1);
+    string last_letter = receiver.ReceiverName.substr(receiver.ReceiverName.length() - 1, 1);
+    string contact_digits = receiver.ReceiverContact.substr(receiver.ReceiverContact.length() - 2, 2);
+    string organ_blood_type = receiver.OrganType.substr(0, 1) + receiver.ReceiverBloodType.substr(0, 1);
 
-    if (registrationChoice == 'Y' || registrationChoice == 'y') {
-        system("CLS");
-        system("color 3F"); // Change console color
+    // Combine components to form ReceiverID
+    string receiver_id = first_letter + last_letter + contact_digits + organ_blood_type;
 
-        std::ofstream file("donor.csv", std::ios::app);
-        if (!file.is_open()) {
-            std::cerr << "Error opening donor.csv for writing.\n";
-            return 1;
-        }
-
-        runDonorRegistrationSystem();
-
-        file.close();
-        std::cout << "Registered successfully!\n";
-        system("CLS");
-        system("color 2F"); // Change console color to default
-
-        std::cout << "Do you want to go to the Donor Registration System or Main Menu?\n";
-        std::cout << "1. Donor Registration System\n";
-        std::cout << "2. Main Menu\n";
-        int menuChoice;
-        std::cin >> menuChoice;
-
-        if (menuChoice == 1) {
-            runDonorRegistrationSystem();
-        } else if (menuChoice == 2) {
-            system("CLS");
-            system("welcome_page.exe"); // Assuming welcome_page.exe is the main menu executable
-        } else {
-            std::cout << "Invalid choice. Returning to Main Menu.\n";
-            system("CLS");
-            system("welcome_page.exe"); // Assuming welcome_page.exe is the main menu executable
-        }
-    } else {
-        std::cout << "Returning to Main Menu.\n";
-        system("CLS");
-        system("welcome_page.exe"); // Assuming welcome_page.exe is the main menu executable
+    // Convert to uppercase
+    for (char &c : receiver_id) {
+        c = toupper(c);
     }
 
-    return 0;
+    return receiver_id;
 }
 
-void registerDonor(std::ofstream& file, Donor& newDonor) {
-    std::cout << "Enter donor details:\n";
-    std::cout << "Name: ";
-    std::cin >> newDonor.name;
-
-    std::cout << "Organ Type: ";
-    std::cin >> newDonor.organType;
-
-    std::cout << "Contact: ";
-    std::cin >> newDonor.contact;
-
-    std::cout << "Age: ";
-    std::cin >> newDonor.age;
-
-    std::cout << "Blood Type: ";
-    std::cin >> newDonor.bloodType;
-
-    std::cout << "Registration Date: ";
-    std::cin >> newDonor.regDate;
-
-    std::cout << "City: ";
-    std::cin >> newDonor.city;
-
-    std::cout << "State: ";
-    std::cin >> newDonor.state;
-
-    std::cout << "Country: ";
-    std::cin >> newDonor.country;
-
-    std::cout << "Address: ";
-    std::cin.ignore();
-    std::getline(std::cin, newDonor.address);
-
-    std::cout << "Zip Code: ";
-    std::cin >> newDonor.zipCode;
-
-    std::cout << "Medical History: ";
-    std::cin.ignore();
-    std::getline(std::cin, newDonor.medicalHistory);
-
-    std::cout << "Insurance: ";
-    std::cin >> newDonor.insurance;
-
-    std::cout << "Consent Form: ";
-    std::cin >> newDonor.consentForm;
-
-    // Print entered details for confirmation
-    printDonorDetails(newDonor);
-
-    // Ask for confirmation
-    if (confirmRegistration()) {
-        // Write details to the file
-        file << newDonor.name << "," << newDonor.organType << "," << newDonor.contact << ","
-             << newDonor.age << "," << newDonor.bloodType << ","
-             << newDonor.regDate << "," << newDonor.city << "," << newDonor.state << ","
-             << newDonor.country << "," << newDonor.address << "," << newDonor.zipCode << ","
-             << newDonor.medicalHistory << "," << newDonor.insurance << ","
-             << newDonor.consentForm << "\n";
-    }
-}
-
-void printDonorDetails(const Donor& donor) {
-    std::cout << "\nDonor Details:\n";
-    std::cout << "Name: " << donor.name << "\n";
-    std::cout << "Organ Type: " << donor.organType << "\n";
-    std::cout << "Contact: " << donor.contact << "\n";
-    std::cout << "Age: " << donor.age << "\n";
-    std::cout << "Blood Type: " << donor.bloodType << "\n";
-    std::cout << "Registration Date: " << donor.regDate << "\n";
-    std::cout << "City: " << donor.city << "\n";
-    std::cout << "State: " << donor.state << "\n";
-    std::cout << "Country: " << donor.country << "\n";
-    std::cout << "Address: " << donor.address << "\n";
-    std::cout << "Zip Code: " << donor.zipCode << "\n";
-    std::cout << "Medical History: " << donor.medicalHistory << "\n";
-    std::cout << "Insurance: " << donor.insurance << "\n";
-    std::cout << "Consent Form: " << donor.consentForm << "\n";
-}
-
-bool confirmRegistration() {
-    char confirmation;
-    std::cout << "\nDo you want to confirm your registration? (Y/N): ";
-    std::cin >> confirmation;
-
-    return (confirmation == 'Y' || confirmation == 'y');
-}
-
-void runDonorRegistrationSystem() {
-    
-    system("CLS");
-    system("color 3F");
-    Donor newDonor;
-    std::ofstream file("donor.csv", std::ios::app);
-
-    if (!file.is_open()) {
-        std::cerr << "Error opening donor.csv for writing.\n";
+void write_to_csv(const ReceiverDetails& receiver) {
+    ofstream csvfile(CSV_FILE_PATH, ios::app);
+    if (!csvfile.is_open()) {
+        cerr << "Error: Could not open the CSV file for writing." << endl;
         return;
     }
 
-    // Initialize the registration date (assuming the system date can be retrieved)
-    // Add other necessary initializations
-    // ...
 
-    registerDonor(file, newDonor);
 
-    file.close();
+    // Generate ReceiverID
+    ReceiverDetails updatedReceiver = receiver;
+    updatedReceiver.ReceiverID = generate_receiver_id(updatedReceiver);
+
+    // Check if ReceiverID already exists
+    for (const auto& entry : receiverDatabase) {
+        if (entry.ReceiverID == updatedReceiver.ReceiverID) {
+            cout << "Your request is under process. A receiver with the same ReceiverID already exists." << endl;
+            return;
+        }
+    }
+
+    // Get current date and time
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
+    stringstream registration_date;
+    registration_date << setw(2) << setfill('0') << ltm->tm_mday << '/'
+                     << setw(2) << setfill('0') << 1 + ltm->tm_mon << '/'
+                     << 1900 + ltm->tm_year;
+
+    updatedReceiver.ReceiverRegistrationDate = registration_date.str();
+
+    // Append the updated receiver details to the CSV file
+    csvfile << updatedReceiver.ReceiverID << ","
+            << updatedReceiver.ReceiverName << ","
+            << updatedReceiver.ReceiverContact << ","
+            << updatedReceiver.OrganType << ","
+            << updatedReceiver.ReceiverAge << ","
+            << updatedReceiver.ReceiverBloodType << ","
+            << updatedReceiver.ReceiverAddress << ","
+            << updatedReceiver.ReceiverCity << ","
+            << updatedReceiver.ReceiverState << ","
+            << updatedReceiver.ReceiverCountry << ","
+            << updatedReceiver.ReceiverZipCode << ","
+            << updatedReceiver.ReceiverMedicalHistory << ","
+            << updatedReceiver.ReceiverInsurance << ","
+            << updatedReceiver.ReceiverConsentForm << ","
+            << updatedReceiver.ReceiverRegistrationDate << "\n";
+
+    // Add the updated receiver details to the in-memory database
+    receiverDatabase.push_back(updatedReceiver);
+
+    cout << "Registration successful. Details have been saved to " << CSV_FILE_PATH << endl;
+}
+
+int main() {
+    ReceiverDetails receiver;
+
+    cout << "Welcome to Organ Receiver Registration" << endl;
+
+    cout << "Register as an organ receiver? (yes/no): ";
+    string response;
+    cin >> response;
+
+    if (response == "yes") {
+        cout << "Enter Receiver Name: ";
+        cin.ignore();
+        getline(cin, receiver.ReceiverName);
+
+        cout << "Enter Receiver Contact: ";
+        getline(cin, receiver.ReceiverContact);
+
+        cout << "Enter Organ Type: ";
+        getline(cin, receiver.OrganType);
+
+        cout << "Enter Receiver Age: ";
+        getline(cin, receiver.ReceiverAge);
+
+        cout << "Enter Receiver Blood Type: ";
+        getline(cin, receiver.ReceiverBloodType);
+
+        cout << "Enter Receiver Address: ";
+        getline(cin, receiver.ReceiverAddress);
+
+        cout << "Enter Receiver City: ";
+        getline(cin, receiver.ReceiverCity);
+
+        cout << "Enter Receiver State: ";
+        getline(cin, receiver.ReceiverState);
+
+        cout << "Enter Receiver Country: ";
+        getline(cin, receiver.ReceiverCountry);
+
+        cout << "Enter Receiver Zip Code: ";
+        getline(cin, receiver.ReceiverZipCode);
+
+        cout << "Enter Receiver Medical History: ";
+        getline(cin, receiver.ReceiverMedicalHistory);
+
+        cout << "Enter Receiver Insurance: ";
+        getline(cin, receiver.ReceiverInsurance);
+
+        cout << "Enter Receiver Consent Form: ";
+        getline(cin, receiver.ReceiverConsentForm);
+
+        // Write receiver details to CSV file and update in-memory database
+        write_to_csv(receiver);
+    } else {
+        cout << "Registration cancelled. Goodbye!" << endl;
+    }
+
+    // Access in-memory database (vector) if needed
+    for (const auto& entry : receiverDatabase) {
+        // Do something with each entry in the in-memory database
+        cout << "ReceiverID: " << entry.ReceiverID << ", ReceiverName: " << entry.ReceiverName << endl;
+    }
+
+    return 0;
 }
